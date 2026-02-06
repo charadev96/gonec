e3cdfac547a676de4d8fb5a025f2f3740d83c05c
charadev96 <charadev96@gmail.com>
2026-02-06 22:39:46 +0300

    Feat: Add module separation to logging

--- internal/shared/log.go
+++ internal/shared/log.go
@@ -3,25 +3,39 @@ package shared
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/rs/zerolog"
 )
 
-func Logger() zerolog.Logger {
-	output := zerolog.ConsoleWriter{
-		Out:        os.Stderr,
-		TimeFormat: "15:04",
+func New(module string) zerolog.Logger {
+	out := zerolog.ConsoleWriter{
+		Out:           os.Stderr,
+		TimeFormat:    "15:04",
+		PartsOrder:    []string{"time", "level", "module", "message"},
+		FieldsExclude: []string{"module"},
 	}
-	output.FormatFieldName = func(i any) string {
+
+	out.FormatPartValueByName = func(i any, s string) string {
+		if s == "module" && i != nil {
+			return strings.ToUpper(fmt.Sprintf("%s", i))
+		}
+		return ""
+	}
+
+	out.FormatFieldName = func(i any) string {
 		return fmt.Sprintf("\n         \033[30m- \033[36m%s: \033[0m", i)
 	}
-	output.FormatErrFieldName = func(i any) string {
+
+	out.FormatErrFieldName = func(i any) string {
 		return fmt.Sprintf("\n         \033[30m- \033[31m%s: \033[0m", i)
 	}
 
-	logger := zerolog.New(output).
+	logger := zerolog.New(out).
 		With().
 		Timestamp().
+		Str("module", module).
 		Logger()
+
 	return logger
 }
