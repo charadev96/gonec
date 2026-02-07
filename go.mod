bbb9b6c3158046c44f764b842775ff7078dc4199
charadev96 <charadev96@gmail.com>
2026-02-07 21:10:43 +0300

    Feat: Add gRPC handler for server admin services

--- go.mod
+++ go.mod
@@ -11,6 +11,9 @@ require (
 	github.com/uptrace/bun v1.2.16
 	github.com/uptrace/bun/dialect/sqlitedialect v1.2.16
 	github.com/uptrace/bun/driver/sqliteshim v1.2.16
+	golang.org/x/sync v0.18.0
+	google.golang.org/grpc v1.78.0
+	google.golang.org/protobuf v1.36.11
 )
 
 require (
@@ -27,7 +30,10 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	golang.org/x/exp v0.0.0-20251113190631-e25ba8c21ef6 // indirect
+	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
 	modernc.org/libc v1.67.0 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
 	modernc.org/memory v1.11.0 // indirect
---
+++ internal/server/handler/admin/user.go
@@ -0,0 +1,143 @@
+package admin
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/jinzhu/copier"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/charadev96/gonec/api/admin/gen"
+	server "github.com/charadev96/gonec/internal/server/domain"
+	"github.com/charadev96/gonec/internal/server/service"
+)
+
+// TODO: Sanitize errors
+
+type UserServiceHandler struct {
+	gen.UnimplementedUserServiceServer
+	Service *service.UserService
+}
+
+func (h *UserServiceHandler) CreateUser(ctx context.Context, req *gen.CreateUserRequest) (*gen.CreateUserReply, error) {
+	id, err := h.Service.Users.Create(ctx)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &gen.CreateUserReply{Id: id.String()}, nil
+}
+
+func (h *UserServiceHandler) CreateInvite(ctx context.Context, req *gen.CreateInviteRequest) (*gen.CreateInviteReply, error) {
+	id, err := uuid.Parse(req.Id)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	opts := service.CreateInviteOptions{
+		NotBefore: req.NotBefore.AsTime(),
+		NotAfter:  req.NotAfter.AsTime(),
+	}
+	inv, err := h.Service.CreateInvite(ctx, id, opts)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	i := new(gen.UserInvite)
+	copier.Copy(i, &inv)
+	i.NotBefore = timestamppb.New(inv.NotBefore)
+	i.NotAfter = timestamppb.New(inv.NotAfter)
+	return &gen.CreateInviteReply{Invite: i}, nil
+}
+
+func (h *UserServiceHandler) GetUserByID(ctx context.Context, req *gen.GetByIDRequest) (*gen.GetUserReply, error) {
+	id, err := uuid.Parse(req.Id)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	user, err := h.Service.Users.GetByID(ctx, id)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	u := new(gen.User)
+	copier.Copy(u, &user)
+	return &gen.GetUserReply{User: u}, nil
+}
+
+func (h *UserServiceHandler) GetUserByName(ctx context.Context, req *gen.GetByNameRequest) (*gen.GetUserReply, error) {
+	user, err := h.Service.Users.GetByName(ctx, req.Name)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	u := new(gen.User)
+	copier.Copy(u, &user)
+	return &gen.GetUserReply{User: u}, nil
+}
+
+func (h *UserServiceHandler) GetInviteByUserID(ctx context.Context, req *gen.GetByIDRequest) (*gen.GetInviteReply, error) {
+	id, err := uuid.Parse(req.Id)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	invite, err := h.Service.Invites.GetByUserID(ctx, id)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	i := new(gen.UserInvite)
+	copier.Copy(i, &invite)
+	return &gen.GetInviteReply{Invite: i}, nil
+}
+
+func (h *UserServiceHandler) ListUsers(ctx context.Context, req *gen.ListUsersRequest) (*gen.ListUsersReply, error) {
+	var cursor uuid.UUID
+	if req.Cursor != "" {
+		var err error
+		cursor, err = uuid.Parse(req.Cursor)
+		if err != nil {
+			return nil, status.Error(codes.InvalidArgument, err.Error())
+		}
+	}
+	query := server.UserListQuery{
+		Limit:  int(req.Limit),
+		Cursor: cursor,
+	}
+	list, err := h.Service.Users.List(ctx, query)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	var users []*gen.User
+	for _, u := range list.Users {
+		users = append(users, &gen.User{})
+		copier.Copy(users[len(users)-1], &u)
+	}
+
+	return &gen.ListUsersReply{
+		Users:  users,
+		Cursor: list.Cursor.String(),
+	}, nil
+
+}
+
+func (h *UserServiceHandler) DeleteUser(ctx context.Context, req *gen.DeleteRequest) (*gen.DeleteReply, error) {
+	id, err := uuid.Parse(req.Id)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	err = h.Service.DeleteUser(ctx, id)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &gen.DeleteReply{}, nil
+}
+
+func (h *UserServiceHandler) DeleteInvite(ctx context.Context, req *gen.DeleteRequest) (*gen.DeleteReply, error) {
+	id, err := uuid.Parse(req.Id)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	err = h.Service.Invites.Delete(ctx, id)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &gen.DeleteReply{}, nil
+}
